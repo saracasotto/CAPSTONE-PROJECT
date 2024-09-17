6425a23305cbd51a63be6e76ee8b810f2f96ffa3
saracasotto <saracasotto.dev@gmail.com>
2024-09-17 18:57:30 +0100

    auth fix

--- moodlit-backend/middleware/authentication.js
+++ moodlit-backend/middleware/authentication.js
@@ -6,12 +6,10 @@ const JWT_SECRET = process.env.JWT_SECRET;
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  // Controllo che l'header di autorizzazione esista
   if (!authHeader) {
     return res.status(401).json({ error: 'Missing token' });
   }
 
-  // Verifica che l'header contenga "Bearer <token>"
   const parts = authHeader.split(' ');
   if (parts.length !== 2 || parts[0] !== 'Bearer') {
     return res.status(401).json({ error: 'Malformed Token' });
@@ -20,17 +18,16 @@ export default async (req, res, next) => {
   const token = parts[1];
 
   try {
-    // Decodifica e verifica il token JWT
-    const payload = jwt.verify(token, JWT_SECRET);
-    console.log('Token payload: ', payload); //Controllo payload
 
-    //Check esistenza utente
+    const payload = jwt.verify(token, JWT_SECRET);
+    console.log('Token payload: ', payload); 
+
+
     const user = await User.findById(payload.id).select('-password');
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
 
-    // Aggiungi l'utente verificato alla richiesta per l'uso nelle rotte successive
     req.loggedUser = user;
 
     next();
